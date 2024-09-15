64f4cb2615ee5eeb9d373497608951a1607bdea4
uzz-ptl7 <uzair07patel@gmail.com>
2024-09-15 13:27:31 +0200

    Understanding properties and methods

--- v-1.js
+++ v-1.js
@@ -16,6 +16,7 @@ class Task {
         console.log(`\t Category: ${this.category}`);
         console.log(`\t Task: ${this.title}`);
         console.log(`\t Description: ${this.description}`);
+        
         // console.log(`\t Completed: ${this.completed}`);
         // if (this.completed) {
         //     console.log("\t Task is Completed");
@@ -30,11 +31,40 @@ class Task {
 
         console.log("+---------------------------------------------------------------+")
     }
+
+    // updateTask(newTitle, newDescription, newCategory) {
+    //     this.title = newTitle
+    //     this.description = newDescription
+    //     this.category = newCategory
+    //     console.log("Task Updated Successfully");   
+    // }
+
+    //...........................Increase User Flexibility............................
+    updateTaskTitle(newTitle) {
+        this.title = newTitle
+        console.log("Task Title Updated Successfully");   
+    }
+
+    updateTaskDescription(newDescription) {
+        this.description = newDescription
+        console.log("Task Description Updated Successfully");   
+    }
+
+    updateTaskCategory(newCategory) {
+        this.category = newCategory
+        console.log("Task Category Updated Successfully");   
+    }
 }
 
 let task1 = new Task("Buy Groceries", "Get milk, eggs and bread from the store.", "Shopping");
 
+//.................Calling the Functions......................
 task1.displayDetails();
+
+task1.updateTaskTitle("Buy Groceries - 1");
+console.log("\t Updated Task Details \t");
+task1.displayDetails();
+
 task1.toogleCompleted();
 console.log("\t New Task Details \t");
 task1.displayDetails();
